aa8a31d89da65cb41cc361253b69576b19860eca
Victor Pacheco <vpacheco26@yahoo.com.br>
2023-08-22 01:11:10 -0300

    adicionando radape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [ // a parte de declarations é para declarar todao os componentes, direrivas, pipes e etc... 
     AppComponent, 
-    CabecalhoComponent,
+    CabecalhoComponent, RodapeComponent,
   ],
   imports: [  // aqui é declarado os modulos que vou usar nesse aqui, seja de biblioteca ou da minha aplicacao 
     BrowserModule,
